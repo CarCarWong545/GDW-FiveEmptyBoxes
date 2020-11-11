798a78a0eb99e270d9fa4e35f37a68f5404c1737
Gohembo <gohembo@gmail.com>
2020-11-11 13:52:03 -0500

    pretty much A1
    no triggers, easy to make a level

---
+++ SpriteLib3.0-v2.0/Trigger.cpp
@@ -0,0 +1,17 @@
+#include "Trigger.h"
+
+void Trigger::OnTrigger()
+{
+	//make the object linked transparent
+	ECS::GetComponent<Sprite>(m_entity).SetTransparency(0.2f);
+}
+
+void Trigger::SetEntity(int entity)
+{
+	m_entity = entity;
+}
+
+int Trigger::GetEntity()
+{
+	return m_entity;
+}
